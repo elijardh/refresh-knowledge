9c2153489882cd0606e3243725c0d4cd29a24c0f
Dare <58129730+elijardh@users.noreply.github.com>
2023-03-04 07:38:42 +0100

    fix: code base cleaning

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,4 @@
 import express, {Application, Request, Response } from "express";
-import * as ItemService from "../services/items.services";
-import { BaseItem,Item } from "../models/item.interface";
 import { createUserHandler } from "../controller/user.controller";
 import Responses from "../middlewares/responses";
 
@@ -21,19 +19,6 @@ const app: Application = express();
 class Routes {
     public router = (app: Application) : any => {
 
-        // Returns all items
-        app.get("/", async (req: Request, res:Response) => {
-
-        try {
-            const items: Item[] = await ItemService.findAll();
-
-            res.status(200).send(items);
-            
-        } catch (error) {
-            res.status(500).send(error);
-        }
-            
-        });
 
         //create a user endpoint
         //validate(createUserSchema) validates the body of the request
@@ -49,92 +34,20 @@ class Routes {
         //delete session
         app.delete("/api/session",requireUser, deleteSessionHandler );
 
+        //create products
         app.post("/api/products",[requireUser,validate(createProductSchema)], createProductHandler);
 
-        app.put("/api/products/:productID",[requireUser,validate(updateProductSchema)], updateProductHandler);
+        //update profuct
+        app.put("/api/products/:productID",[requireUser,validate(updateProductSchema)], updateProductHandler); 
 
+        //get products
         app.get("/api/products/:productID",validate(getProductSchema), getProductHandler);
 
+        //delete products
         app.delete("/api/products/:productID",[requireUser,validate(deleteProductSchema)], deleteProductHandler);
 
 
-        app.get("/:id",async (req: Request, res:Response) =>{
-
-            const id:number = parseInt(req.params.id,10);
-
-            try {
-
-                const item:Item|null = await ItemService.find(id);
-
-                if (item == null) {
-                    res.status(200).send({"error":"Not found"});
-                      return;
-                }
-                res.status(200).send(item);
-
-                
-            } catch (error) {
-                res.status(500).send(error);
-           
-                
-            }
-           
-        });
-
-        app.post("/create", async (req:Request, res:Response)=>{
-            try {
-
-            
-               const item: BaseItem =req.body;
-
-               const newItem = await ItemService.create(item);
-
-               res.status(201).json(newItem);
-
-            } catch (error) {
-                res.status(500).send(error);
-                
-            }
-        });
-        app.put("/update/:id", async (req:Request,res:Response)=>{
-       const id: number =parseInt(req.params.id, 10);
-
-       try {
-        const itemUpdate:Item = req.body;
-
-        const existingItem:Item|null = await ItemService.find(id);
-
-        if (existingItem == null) {
-            res.status(200).send({"error":"Not found"});
-            return;
-        }
-
-        if (existingItem) {
-            const updatedItem = await ItemService.update(itemUpdate,id);
-
-            res.status(200).json(updatedItem);
-            return;
-        }
-
-        const newItem =await ItemService.create(itemUpdate);
-
-        res.status(200).json(newItem);
-       } catch (error) {
-        res.status(500).send(error);
-       }
-        });
-
-        app.post("/delete/:id",async (req:Request, res:Response) =>{
-            try {
-                const id: number =parseInt(req.params.id);
-                await ItemService.remove(id);
-
-                res.sendStatus(200);
-            } catch (error) {
-
-                 res.status(500).send(error);
-            }
-        });
+   
     };
 
 
